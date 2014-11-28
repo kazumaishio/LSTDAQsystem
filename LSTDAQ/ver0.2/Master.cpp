45e1a6d42dc360cfa007ca8338eefb8a3e88050d
Kazuma Ishio <kazumaishio@gmail.com>
2014-11-28 10:40:59 +0900

    ver0.2 and ver0.3
    ver0.2 : simple "RingBuffer" class test. Instead of using struct or shared scope, Class file begin to be used
    ver0.3 : TCPClientSocket class test. with simple main func to check this class.

---
+++ LSTDAQ/ver0.2/Master.cpp
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <pthread.h>
+
+#include <iostream>
+#include <string>
+#include <unistd.h>//for sleep()
+#include "RingBuffer.hpp"
+
+//unsigned int __g_buff[128];//receive buffer
+using namespace std;
+
+void *Collector_thread(void *arg)
+{
+  LSTDAQ::RingBuffer *rb = (LSTDAQ::RingBuffer*)arg;
+while(1)
+{
+
+  char tempdata[128];
+  cin>>tempdata;
+　cin.clear();
+
+  int outcount;
+  rb->write((unsigned int *)tempdata);
+}
+
+}
+
+void *Builder_thread(void *arg)
+{
+  char outdata[128];
+  LSTDAQ::RingBuffer *rb = (LSTDAQ::RingBuffer*)arg;
+while(1)
+{
+
+  rb->read((unsigned int*)outdata);
+  cout<<outdata<<endl;
+}
+
+
+}
+
+
+int main()
+{
+  //  using namespace std;
+　pthread_t handle[2];
+　
+// 　pthread_mutex_t mutex;
+  LSTDAQ::RingBuffer *rb;
+  // pthread_mutex_init(&mutex, NULL);
+  pthread_create(&handle[0],
+                   NULL,
+                   &Builder_thread,
+                   (void*)rb);
+  pthread_create(&handle[1],
+                   NULL,
+                   &Collector_thread,
+                   (void*)rb);
+  for(int i=0;i<2;i++)
+    pthread_join(handle[i],NULL);
+
+  delete rb;
+
+
+
+  std::cout<<"Hello world!"<<std::endl;
+  return 0;
+}
+
