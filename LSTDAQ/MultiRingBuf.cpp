43fd358ce292f96261dd9a44730d43ff23d46b54
Kazuma Ishio <kazumaishio@gmail.com>
2014-11-29 17:04:48 +0900

    multi RingBuufer test ver0.4
    Succeeded in using multi thread
    Collector writes massage to RingBuffer and
    Builder reads from it.
    Builder doesn't assemble the RingBuffers yet.

--- LSTDAQ/MultiRingBuf.cpp
+++ LSTDAQ/MultiRingBuf.cpp
@@ -1,105 +1,134 @@
 #include <iostream> //cout
 #include <string.h> //strcpy
 #include <stdio.h>//sprintf
-
+#include <unistd.h>//for sleep()
+#include <pthread.h>
 #include "RingBuffer.hpp"
 using namespace std;
 
+#define MAX_RINGBUF 100
 /****************************/
 // struct definition
 /****************************/
 struct sRingBuffer{
   int sRBid;
+  char szAddr[18];//="192.168.10.1"
+  unsigned short shPort;// = 24;
   LSTDAQ::RingBuffer* rb ;
   sRingBuffer* next;
 };
-
-sRingBuffer sRB[10];
+sRingBuffer sRB[MAX_RINGBUF];
 //void sRBinit();
 //void sRBadd();
 
 void sRBinit()
 {
-  for(int i=0; i<10; i++)
+  for(int i=0; i<MAX_RINGBUF; i++)
   {
     sRB[i].sRBid = -1;
     sRB[i].next = 0;
   }
 }
+
+void sRBcreate(int Nconn)
+{
+  for(int i=0; i<Nconn; i++)
+  {
+    sRB[i].sRBid = i;
+    sRB[i].rb = new LSTDAQ::RingBuffer();
+    sRB[i].next=&sRB[i]
+  }
+}
+
 //void sRBadd(LSTDAQ::RingBuffer* rb)
 //{
 //  sRBins(rb,9999);
 //}
-//void sRBins(LSTDAQ::RingBuffer* rb)
+//void sRBinsert(LSTDAQ::RingBuffer* rb)
 //{
 //  sRingBuffer *freap, *insp;
 //  int last;
 //  freep =
 //}
 
+
 /****************************/
 // thread definition
 /****************************/
 
-void otherfunc(void *arg)
+void *Collector_thread(void *arg)
+{
+  char tempbuf[976];
+  sRingBuffer *srb = (sRingBuffer*)arg;
+
+  sprintf(tempbuf,"Hello World!%d",srb->sRBid);
+  srb->rb->write((void*)tempbuf);
+  //sleep(1);
+  cout << "Coll"<<srb->sRBid <<" wrote :"<< tempbuf <<endl;
+  
+}
+
+void *Builder_thread(void *arg)
 {
   char tempbuf[976];
   sRingBuffer *srb = (sRingBuffer*)arg;
   
-  //for(int i=0; i<3;i++)
-    {
-      srb->rb->read((void*)tempbuf);
-    }
-
-  //for(int i=0; i<3;i++)
-    {
-      cout << tempbuf <<endl;
-    }
-
+  srb->rb->read((void*)tempbuf);
+  //sleep(1);
+  cout << "Bld"<<srb->sRBid <<" read :"<< tempbuf <<endl;
+  
 }
-
 /****************************/
 // main definition
 /****************************/
 
 int main()
 {
-  sRBinit();
+
   char tempbuf[976];
   char tempbuf2[3][976];
   strcpy(tempbuf,"Hello world!!init");
   cout << tempbuf <<endl;
+  //Reads Connection.conf
+  int Nconn=2;
+  sRBinit();
+  sRBcreate(Nconn);
+//  for (int i =0; i< Nconn; i++)
+//  {
+//    sRB[i].rb = new LSTDAQ::RingBuffer();
+//  }
   
-  //object array test.
-  LSTDAQ::RingBuffer *rb[3];
-  for(int i=0;i<3;i++)rb[i]= new LSTDAQ::RingBuffer();
-   for(int i=0; i<3;i++)
-     {
-       sprintf(tempbuf,"Array of object test  : Hello World!%d",i);
-       rb[i]->write(tempbuf);
-     }
   
-   for(int i=0; i<3;i++)
-     {
-       rb[i]->read(tempbuf2[i]);
-       cout << tempbuf2[i] <<endl;
-     }
   //struct test
   //giving struct to another function which only receives void* arg
   cout << "*** Object transfer test ***"<<endl;
-  for (int i =0; i< 3; i++)
-    {
-      sRB[i].rb = new LSTDAQ::RingBuffer();
-    }
-  for(int i=0; i<3;i++)
+  for(int i=0; i<Nconn;i++)
     {
       sprintf(tempbuf,"Hello World!%d",i);
       sRB[i].rb->write(tempbuf);
-//      sRBadd(rb[i]);
-//      sRB[i].rb = new LSTDAQ::RingBuffer();
-      otherfunc(&sRB[i]);
     }
-  //RingBufArray rba;
+  //Multi-threaded process creation
+  pthread_t handle[2];
+  for(int i=0;i<Nconn;i++)
+  {
+    pthread_create(&handle[i],
+                   NULL,
+                   &Collector_thread,
+                   &sRB[i]);
+    pthread_create(&handle[i],
+                    NULL,
+                    &Builder_thread,
+                    &sRB[i]);
 
-  return 0; 
+  }
+//  pthread_create(&handle[0],
+//                 NULL,
+//                 &Builder_thread,
+//                 &sRB[0]);
+  for(int i=0;i<Nconn;i++)
+    pthread_join(handle[i],NULL);
+  
+  strcpy(tempbuf,"Hello world!!end");
+  cout << tempbuf <<endl;
+  return 0;
 }
