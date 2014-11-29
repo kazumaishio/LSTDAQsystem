1962ba861093257d7042bd833d933ac5fb15c2f9
Kazuma Ishio <kazumaishio@gmail.com>
2014-11-29 18:43:51 +0900

    multi RingBuffer test ver0.5
    A Builder thread reads data in all RingBuffers

--- LSTDAQ/MultiRingBuf.cpp
+++ LSTDAQ/MultiRingBuf.cpp
@@ -6,7 +6,7 @@
 #include "RingBuffer.hpp"
 using namespace std;
 
-#define MAX_RINGBUF 100
+#define MAX_RINGBUF 10
 /****************************/
 // struct definition
 /****************************/
@@ -27,6 +27,7 @@ void sRBinit()
   {
     sRB[i].sRBid = -1;
     sRB[i].next = 0;
+    cout <<sRB[i].next<<endl;
   }
 }
 
@@ -36,7 +37,8 @@ void sRBcreate(int Nconn)
   {
     sRB[i].sRBid = i;
     sRB[i].rb = new LSTDAQ::RingBuffer();
-    sRB[i].next=&sRB[i]
+    sRB[i].next=&sRB[i+1];
+        cout <<sRB[i].next<<endl;
   }
 }
 
@@ -61,7 +63,7 @@ void *Collector_thread(void *arg)
   char tempbuf[976];
   sRingBuffer *srb = (sRingBuffer*)arg;
 
-  sprintf(tempbuf,"Hello World!%d",srb->sRBid);
+  sprintf(tempbuf,"Hello World!Collector thread wrote %d",srb->sRBid);
   srb->rb->write((void*)tempbuf);
   //sleep(1);
   cout << "Coll"<<srb->sRBid <<" wrote :"<< tempbuf <<endl;
@@ -71,11 +73,30 @@ void *Collector_thread(void *arg)
 void *Builder_thread(void *arg)
 {
   char tempbuf[976];
-  sRingBuffer *srb = (sRingBuffer*)arg;
-  
-  srb->rb->read((void*)tempbuf);
+  sRingBuffer *srb[MAX_RINGBUF];
+  srb[0]= (sRingBuffer*)arg;
+  int Nconn =0;
+  while(1)
+  {
+    cout<<"aaa"<<endl;
+    cout<<"srb["<<Nconn<<"] :"<<srb[Nconn]->next<<endl;
+    if(srb[Nconn]->next==0||Nconn==MAX_RINGBUF)break;
+    srb[Nconn+1]=srb[Nconn]->next;
+    Nconn++;
+  }
+  //read two times each.
+  // one is written by main func
+  // the other is written by Collector_thread
+  for(int j=0;j<2;j++)
+  {
+    for(int i =0;i<Nconn;i++)
+    {
+      srb[i]->rb->read((void*)tempbuf);
+      cout << "Bld read from Coll"<<srb[i]->sRBid <<" :"<< tempbuf <<endl;
+    }
+  }
   //sleep(1);
-  cout << "Bld"<<srb->sRBid <<" read :"<< tempbuf <<endl;
+
   
 }
 /****************************/
@@ -90,42 +111,38 @@ int main()
   strcpy(tempbuf,"Hello world!!init");
   cout << tempbuf <<endl;
   //Reads Connection.conf
-  int Nconn=2;
+  int Nconn=4;
   sRBinit();
   sRBcreate(Nconn);
-//  for (int i =0; i< Nconn; i++)
-//  {
-//    sRB[i].rb = new LSTDAQ::RingBuffer();
-//  }
-  
   
   //struct test
   //giving struct to another function which only receives void* arg
   cout << "*** Object transfer test ***"<<endl;
   for(int i=0; i<Nconn;i++)
     {
-      sprintf(tempbuf,"Hello World!%d",i);
+      sprintf(tempbuf,"Hello World!main thread wrote. %d",i);
       sRB[i].rb->write(tempbuf);
     }
   //Multi-threaded process creation
-  pthread_t handle[2];
+  pthread_t handle[Nconn+1];
   for(int i=0;i<Nconn;i++)
   {
     pthread_create(&handle[i],
                    NULL,
                    &Collector_thread,
                    &sRB[i]);
-    pthread_create(&handle[i],
-                    NULL,
-                    &Builder_thread,
-                    &sRB[i]);
-
+//    pthread_create(&handle[i],
+//                    NULL,
+//                    &Builder_thread,
+//                    &sRB[i]);
+//
   }
-//  pthread_create(&handle[0],
-//                 NULL,
-//                 &Builder_thread,
-//                 &sRB[0]);
-  for(int i=0;i<Nconn;i++)
+  sleep(1);
+  pthread_create(&handle[Nconn],
+                 NULL,
+                 &Builder_thread,
+                 &sRB[0]);
+  for(int i=0;i<Nconn+1;i++)
     pthread_join(handle[i],NULL);
   
   strcpy(tempbuf,"Hello world!!end");
