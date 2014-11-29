9c192da9091f08f6f4b206c87886d27245c8815a
Kazuma Ishio <kazumaishio@gmail.com>
2014-11-29 14:21:54 +0900

    multi RingBuffer test ver0.3
    Succeeded in:
     >Using array of RingBuffer object.
     >Transferring a struct which includes RingBuffer object
    	to a function which receives void* arg

--- LSTDAQ/MultiRingBuf.cpp
+++ LSTDAQ/MultiRingBuf.cpp
@@ -5,61 +5,101 @@
 #include "RingBuffer.hpp"
 using namespace std;
 
-struct RingBufArray{
-  LSTDAQ::RingBuffer *rb[3] ;
+/****************************/
+// struct definition
+/****************************/
+struct sRingBuffer{
+  int sRBid;
+  LSTDAQ::RingBuffer* rb ;
+  sRingBuffer* next;
 };
+
+sRingBuffer sRB[10];
+//void sRBinit();
+//void sRBadd();
+
+void sRBinit()
+{
+  for(int i=0; i<10; i++)
+  {
+    sRB[i].sRBid = -1;
+    sRB[i].next = 0;
+  }
+}
+//void sRBadd(LSTDAQ::RingBuffer* rb)
+//{
+//  sRBins(rb,9999);
+//}
+//void sRBins(LSTDAQ::RingBuffer* rb)
+//{
+//  sRingBuffer *freap, *insp;
+//  int last;
+//  freep =
+//}
+
+/****************************/
+// thread definition
+/****************************/
 
 void otherfunc(void *arg)
 {
   char tempbuf[976];
-  RingBufArray *rba = (RingBufArray*)arg;
+  sRingBuffer *srb = (sRingBuffer*)arg;
   
-  for(int i=0; i<3;i++)
+  //for(int i=0; i<3;i++)
     {
-      (rba->rb[i])->read((void*)tempbuf[i]);
+      srb->rb->read((void*)tempbuf);
     }
 
-  for(int i=0; i<3;i++)
+  //for(int i=0; i<3;i++)
     {
-      cout << tempbuf[i] <<endl;
+      cout << tempbuf <<endl;
     }
 
 }
 
+/****************************/
+// main definition
+/****************************/
+
 int main()
 {
+  sRBinit();
   char tempbuf[976];
   char tempbuf2[3][976];
-  // cout<<" Hello world!!" <<endl;
-  // for(int i =0; i<3; i++)
-  //   {
-
-  //   }
-  strcpy(tempbuf,"Hello world!!nagamenishokika");
-
+  strcpy(tempbuf,"Hello world!!init");
   cout << tempbuf <<endl;
+  
+  //object array test.
   LSTDAQ::RingBuffer *rb[3];
+  for(int i=0;i<3;i++)rb[i]= new LSTDAQ::RingBuffer();
+   for(int i=0; i<3;i++)
+     {
+       sprintf(tempbuf,"Array of object test  : Hello World!%d",i);
+       rb[i]->write(tempbuf);
+     }
+  
+   for(int i=0; i<3;i++)
+     {
+       rb[i]->read(tempbuf2[i]);
+       cout << tempbuf2[i] <<endl;
+     }
+  //struct test
+  //giving struct to another function which only receives void* arg
+  cout << "*** Object transfer test ***"<<endl;
   for (int i =0; i< 3; i++)
     {
-      rb[i] = new LSTDAQ::RingBuffer();
+      sRB[i].rb = new LSTDAQ::RingBuffer();
     }
   for(int i=0; i<3;i++)
     {
       sprintf(tempbuf,"Hello World!%d",i);
-      rb[i]->write(tempbuf);      
+      sRB[i].rb->write(tempbuf);
+//      sRBadd(rb[i]);
+//      sRB[i].rb = new LSTDAQ::RingBuffer();
+      otherfunc(&sRB[i]);
     }
-  // for(int i=0; i<3;i++)
-  //   {
-  //     rb[i]->read(tempbuf2[i]);
-  //   }
+  //RingBufArray rba;
 
-  // for(int i=0; i<3;i++)
-  //   {
-  //     cout << tempbuf2[i] <<endl;
-  //   }
-  RingBufArray rba;
-  for(int i=0;i<3;i++)
-    rba.rb[i] = rb[i];
-  otherfunc(&rba);
   return 0; 
 }
--- LSTDAQ/RingBuffer.cpp
+++ LSTDAQ/RingBuffer.cpp
@@ -4,27 +4,25 @@
 #include <string.h>//memcpy
 #include <stdlib.h>//malloc
 
-//yet read() and write() is to be modified
-//nbytes should not be used
-//While the EVENTSIZE should be variable
+//EVENTSIZE should be variable
 // for multiple connection.
+//m_Nm and EVENTSIZE should be defined more explicitly.
 
 namespace LSTDAQ{
   RingBuffer::RingBuffer() throw():m_Nm(1000)
   {
     //mutex initialization
-   std::cout<<"constructor 1"<<std::endl;
+ 
     m_mutex = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t));
     pthread_mutex_init(m_mutex,NULL);
-   std::cout<<"constructor 2"<<std::endl;
     m_cond = (pthread_cond_t *)malloc(sizeof(pthread_cond_t));
     pthread_cond_init(m_cond,NULL);
     //info initialization
-//    m_Nm=1000;
     m_Nw=0;
     m_Nr=0;
     m_Nmw=0;
     m_Nmr=0;
+    std::cout<<"constructor succeed"<<std::endl;
     //buffer initialization
 //    for (int i=0; i<EVENTSIZE*Nm; i++) {
 //      m_buffer++;
@@ -63,7 +61,7 @@ namespace LSTDAQ{
     if (m_Nmw == m_Nm) {
       m_Nmw=0;
     }
-    std::cout<<"write end "<<m_Nw<<std::endl;
+    std::cout<<"write end "<<std::endl;//"m_Nw = "<<m_Nw<<",m_Nmw = "<<m_Nmw<<std::endl;
     //mutex unlock
     pthread_cond_signal(m_cond);
     pthread_mutex_unlock(m_mutex);
@@ -71,7 +69,7 @@ namespace LSTDAQ{
   }
   unsigned int RingBuffer::read(void *buf)
   {
-    sleep(2);
+    //sleep(2);
     //mutex lock
     pthread_mutex_lock(m_mutex);
     std::cout<<"hello read-->";//<<std::endl;
@@ -83,7 +81,7 @@ namespace LSTDAQ{
     memcpy(buf,m_buffer + EVENTSIZE * m_Nmr,EVENTSIZE);
     m_Nr++;
     m_Nmr++;
-    std::cout<<"read end"<<m_Nr<<std::endl;
+    std::cout<<"read end"<<std::endl;//"m_Nr = "<<m_Nr<<",m_Nmr = "<<m_Nmr<<std::endl;
     if (m_Nmr == m_Nm) {
       m_Nmr=0;
     }
