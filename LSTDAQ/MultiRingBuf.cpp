439c314ae91fc659c770ff63f55c9731a028addc
Kazuma Ishio <kazumaishio@gmail.com>
2014-12-01 15:23:00 +0900

    multi RingBuffer test ver0.6
    With these features:
     DAQtimer
     Reading Connection.conf
     Storing Connection info in structRingBuffer

--- LSTDAQ/MultiRingBuf.cpp
+++ LSTDAQ/MultiRingBuf.cpp
@@ -1,13 +1,21 @@
+#define MAX_RINGBUF 10
+#define MAX_CONNECTION 48
+#define DAQ_NEVENT  3
+
 #include <iostream> //cout
 #include <string.h> //strcpy
+//#include <string>//istringstream
 #include <stdio.h>//sprintf
 #include <unistd.h>//for sleep()
+#include <fstream>//filestream(file I/O)
+#include <sstream>//stringstream
+#include <iomanip>//padding cout
+
 #include <pthread.h>
 #include "RingBuffer.hpp"
+#include "DAQtimer.hpp"
 using namespace std;
 
-#define MAX_RINGBUF 10
-#define DAQ_NEVENT  3
 /****************************/
 // struct definition
 /****************************/
@@ -19,8 +27,10 @@ struct sRingBuffer{
   sRingBuffer* next;
 };
 sRingBuffer sRB[MAX_RINGBUF];
+
 //void sRBinit();
-//void sRBadd();
+//void sRBcreate(int nServ);
+//void sRBsetaddr(int sRBid, char *szAddr, unsigned short shPort);
 
 void sRBinit()
 {
@@ -32,9 +42,9 @@ void sRBinit()
   }
 }
 
-void sRBcreate(int Nconn)
+void sRBcreate(int nServ)
 {
-  for(int i=0; i<Nconn; i++)
+  for(int i=0; i<nServ; i++)
   {
     sRB[i].sRBid = i;
     sRB[i].rb = new LSTDAQ::RingBuffer();
@@ -43,16 +53,14 @@ void sRBcreate(int Nconn)
   }
 }
 
-//void sRBadd(LSTDAQ::RingBuffer* rb)
-//{
-//  sRBins(rb,9999);
-//}
-//void sRBinsert(LSTDAQ::RingBuffer* rb)
-//{
-//  sRingBuffer *freap, *insp;
-//  int last;
-//  freep =
-//}
+void sRBsetaddr(int sRBid, char *szAddr, unsigned short shPort)
+{
+  if(sRB[sRBid].sRBid==sRBid)
+  {
+    strcpy(sRB[sRBid].szAddr, szAddr);
+    sRB[sRBid].shPort=shPort;
+  }
+}
 
 
 /****************************/
@@ -63,7 +71,6 @@ void *Collector_thread(void *arg)
 {
   char tempbuf[976];
   sRingBuffer *srb = (sRingBuffer*)arg;
-  
   sprintf(tempbuf,"Hello World!Collector thread wrote %d",srb->sRBid);
   for(int i = 0; i< DAQ_NEVENT;i++)
     srb->rb->write((void*)tempbuf);
@@ -74,35 +81,41 @@ void *Collector_thread(void *arg)
 
 void *Builder_thread(void *arg)
 {
-
-  char tempbuf[976];
   sRingBuffer *srb[MAX_RINGBUF];
   srb[0]= (sRingBuffer*)arg;
+  char tempbuf[976];
   unsigned long long ReadBytes = DAQ_NEVENT * 976;
 
-  int Nconn =0;
-  while(1)
-  {
-    cout<<"aaa"<<endl;
-    cout<<"srb["<<Nconn<<"] :"<<srb[Nconn]->next<<endl;
-    if(srb[Nconn]->next==0||Nconn==MAX_RINGBUF)break;
-    srb[Nconn+1]=srb[Nconn]->next;
-    Nconn++;
-  }
-  unsigned long long llRead[MAX_RINGBUF] = {0};
-  while(1)
-  {
-    for(int i =0;i<Nconn;i++)
-    {
-      llRead[i] +=srb[i]->rb->read((void*)tempbuf);
-      cout << "Bld read from Coll"<<srb[i]->sRBid <<" :"<< tempbuf <<endl;
-    
-    }
-      if(llRead[Nconn-1]>=ReadBytes)break;
-  }
-  //sleep(1);
-
+  //variables for measurement
+  unsigned long long llMesReadBytes[MAX_RINGBUF] = {0};
   
+  cout<<"*** Builder_thread initialization ***"<<endl;
+  int nColl =0;
+  while(1)
+  {
+    cout<<"srb["<<nColl<<"] :"<<srb[nColl]->next<<endl;
+    if(srb[nColl]->next==0||nColl==MAX_RINGBUF)break;
+    srb[nColl+1]=srb[nColl]->next;
+    nColl++;
+  }
+  LSTDAQ::DAQtimer *dt=new LSTDAQ::DAQtimer(nColl);
+  dt->DAQstart();
+  //measurement starts here
+  while(1)
+  {
+    for(int i =0;i<nColl;i++)
+    {
+      llMesReadBytes[i] +=srb[i]->rb->read((void*)tempbuf);
+      dt->readend();
+      //cout << "Bld read from Coll"<<srb[i]->sRBid <<" :"<< tempbuf <<endl;
+      
+    }
+    if(llMesReadBytes[nColl-1]>=ReadBytes)break;
+  }
+  dt->DAQend();
+  dt->DAQsummary();
+  //measurement ends here
+  //sleep(1);
 }
 /****************************/
 // main definition
@@ -112,18 +125,45 @@ int main()
 {
 
   cout << "Hello world!!init" <<endl;
-  //Reads Connection.conf
-  int Nconn=4;
-  sRBinit();
-  sRBcreate(Nconn);
+  /******************************************/
+  //  Reading Connection Configuration
+  /******************************************/
+  char szAddr[MAX_CONNECTION][16];
+  unsigned short shPort[MAX_CONNECTION]={0};
+  unsigned long lConnected=0;
+  const char *ConfFile = "Connection.conf";
+  std::ifstream ifs(ConfFile);
+  std::string str;
+  int nServ=0;
+  while (std::getline(ifs,str)){
+    if(str[0]== '#' || str.length()==0)continue;
+    std::istringstream iss(str);
+    iss >> szAddr[nServ] >> shPort[nServ];
+    nServ++;
+  }
+  if(nServ>MAX_CONNECTION){
+    printf("The number of connections excessed limit.");
+    exit(1);
+  }
   
-  //struct test
-  //giving struct to another function which only receives void* arg
-  cout << "*** Object transfer test ***"<<endl;
-  cout << DAQ_NEVENT << "events will be transferred each"<<endl;
-  //Multi-threaded process creation
-  pthread_t handle[Nconn+1];
-  for(int i=0;i<Nconn;i++)
+  sRBinit();
+  sRBcreate(nServ);
+  for(int i=0;i<nServ;i++)
+    sRBsetaddr(i,szAddr[i],shPort[i]);
+
+  cout<<"****** Configuration of RinbBuffers are set ******"<<endl;
+  cout<<"RBid "<<"    IP address     "<<" port "<<endl;
+  for(int i=0;i<nServ;i++)
+    cout<<"RB"<< setw(2) << setfill(' ')<<i<<"|"<<
+    setw(18) << setfill(' ')<<sRB[i].szAddr<<"|"<<
+    setw(5) << setfill(' ')<<sRB[i].shPort<<endl;
+  
+  /******************************************/
+  //  Multi-threaded process creation
+  /******************************************/
+  cout << "***  Thread create  ***"<<endl;
+  pthread_t handle[nServ+1];
+  for(int i=0;i<nServ;i++)
   {
     pthread_create(&handle[i],
                    NULL,
@@ -131,11 +171,13 @@ int main()
                    &sRB[i]);
   }
   //sleep(1);
-  pthread_create(&handle[Nconn],
+  pthread_create(&handle[nServ],
                  NULL,
                  &Builder_thread,
                  &sRB[0]);
-  for(int i=0;i<Nconn+1;i++)
+  cout <<"Threads created. "<<
+      DAQ_NEVENT << "events will be transferred each"<<endl;
+  for(int i=0;i<nServ+1;i++)
     pthread_join(handle[i],NULL);
   
   cout << "Hello world!!end" <<endl;
