50768e2c45da402aec1987906083d7db37a89691
Kazuma Ishio <kazumaishio@gmail.com>
2014-12-15 12:14:55 +0900

    LSTDAQ ver1.2
    > Creates datafile and stores data
        in which datas of a event from multiple connections
           are combined.
    > Fatal bug on RB write() is fixed.
       The logic to prevent from overwriting is fixed.
    
    > read() in RB is not improved
        in which a multiplication still exists.

---
+++ LSTDAQ/ver1.2/DAQtimer.cpp
@@ -0,0 +1,134 @@
+//****************************************************
+// DAQ timer
+//****************************************************
+#include <time.h>     //for measuring time
+#include <sys/time.h> //for making filename
+#include "DAQtimer.hpp"
+#include "RingBuffer.hpp" //EVENTSIZE
+#include <errno.h>
+#include <ctime>
+#include <unistd.h>   //for inspecting directory
+#include <sys/stat.h> //for making directory
+#include <iostream>
+#include <iomanip> //for padding
+
+#include <fstream>
+#include <sstream>
+///////////////////////////////////////////////////////////////////////////////////////////
+// clock_gettime(CLOCK_REALTIME) is not available on Mac OSX.
+// instead, this is imported from https://gist.github.com/jbenet/1087739
+///////////////////////////////////////////////////////////////////////////////////////////
+//****************************************************
+#ifdef __MACH__
+#include <mach/clock.h>
+#include <mach/mach.h>
+#endif
+
+void current_utc_time(struct timespec *ts) {
+  
+#ifdef __MACH__ // OS X does not have clock_gettime, use clock_get_time
+  clock_serv_t cclock;
+  mach_timespec_t mts;
+  host_get_clock_service(mach_host_self(), CALENDAR_CLOCK, &cclock);
+  clock_get_time(cclock, &mts);
+  mach_port_deallocate(mach_task_self(), cclock);
+  ts->tv_sec = mts.tv_sec;
+  ts->tv_nsec = mts.tv_nsec;
+#else
+  clock_gettime(CLOCK_REALTIME, ts);
+#endif
+}
+//****************************************************
+namespace LSTDAQ{
+  DAQtimer::DAQtimer(int nServ) throw()
+  {
+    m_nServ = nServ;
+    tv.tv_sec = 0;
+    tv.tv_usec = 10000;
+    for(int i=0;i<1000;i++)
+      lltime_diff[i]=0;
+  }
+  DAQtimer::~DAQtimer() throw()
+  {
+  }
+  ///////////////////////////////////////////////////////////////////////////////////////////
+  // time calc
+  ///////////////////////////////////////////////////////////////////////////////////////////
+  //#define TIME_SEC2NSEC	1000000000 //moved to .hpp file
+  unsigned long long DAQtimer::GetRealTimeInterval(const  struct timespec *pFrom, const struct timespec *pTo)
+  {
+    unsigned long long  llStart = (unsigned long long )((unsigned long long )pFrom->tv_sec*TIME_SEC2NSEC + (unsigned long long )pFrom->tv_nsec);
+    unsigned long long  llEnd = (unsigned long long )((unsigned long long )pTo->tv_sec*TIME_SEC2NSEC + (unsigned long long )pTo->tv_nsec);
+    return( (llEnd - llStart)/1000 );
+  }
+  ///////////////////////////////////////////////////////////////////////////////////////////
+  // clock_gettime(CLOCK_REALTIME) is not available on Mac OSX.
+  ///////////////////////////////////////////////////////////////////////////////////////////
+  void DAQtimer::DAQstart()
+  {
+    //clock_gettime(CLOCK_REALTIME,&tsStart);
+    current_utc_time(&tsStart);
+    tsctime1=tsStart;
+    readcount =0;
+  }
+  void DAQtimer::readend()
+  {
+    //clock_gettime(CLOCK_REALTIME,&tsctime2);
+    current_utc_time(&tsctime2);
+    if(readcount<1000)
+    {
+      lltime_diff[readcount] = GetRealTimeInterval(&tsctime1,&tsctime2);
+      tsctime1=tsctime2;
+    }
+    readcount++;
+  }
+  void DAQtimer::DAQend()
+  {
+    //clock_gettime(CLOCK_REALTIME,&tsEnd);
+    current_utc_time(&tsEnd);
+  }
+  void DAQtimer::DAQsummary(int infreq, unsigned long long nEvent,int nRB,int nColl)
+  {
+    unsigned long long llusec = GetRealTimeInterval(&tsStart,&tsEnd);
+    std::cout <<llusec<<std::endl;
+    double readfreq;
+    std::cout<<"By"<<readcount<<"times of reading,"<<std::endl;
+    std::cout<<nEvent<<"events read within "<<llusec/1000000.0<<"sec."<<std::endl;
+    unsigned long long llRead = nEvent * EVENTSIZE;
+    readfreq = (double)nEvent/llusec*1000000.0;
+    readrate = (double)(llRead*8)/llusec*1000000.0/1024./1024.;
+    std::cout<<"Read Freq :"<<readfreq<<"[Hz]"<<std::endl;
+    std::cout<<"Read Rate :"<<readrate<<"[Mbps]"<<std::endl;
+    //**** file create
+    std::ofstream   m_fout;
+    // using namespace std;
+    // m_foutName = MESFILE;
+    // char foutName[128];
+    std::sprintf(m_foutName,MESFILE);
+    m_fout.open(m_foutName,std::ios_base::in | std::ios_base::out | std::ios_base::ate);
+    if (!m_fout) {
+      m_fout.open(m_foutName);
+      m_fout<<"The result of LSTDAQ \n";
+      m_fout<<"nColl nConn InFreq[Hz]  RdFreq[Hz]  RdRate[Mbps]   nEvents   ReadTrial";
+      m_fout<<std::endl;
+      std::cout<<"New measurement file is created. "<<std::endl;
+    }
+    m_fout//<<" "
+    << std::setw(4)<< std::setfill(' ')<< std::fixed<< std::setprecision(0)
+    << nColl            << "  "
+    << std::setw(4)<< std::setfill(' ')<< std::fixed<< std::setprecision(0)
+    << nRB            << "  "
+    << std::setw(6)<< std::setfill(' ')<< std::fixed<< std::setprecision(0)
+    << infreq         << "     "
+    << std::setw(10)  << std::setfill(' ')<< std::fixed<< std::setprecision(3)
+    << readfreq       << "   "
+    << std::setw(10)  << std::setfill(' ')<< std::fixed<< std::setprecision(3)
+    << readrate       << "   "
+    << std::setw(10)  << std::setfill(' ')<< std::fixed<< std::setprecision(0)
+    << nEvent         << "  "
+    << std::setw(10)  << std::setfill(' ')<< std::fixed<< std::setprecision(0)
+    << readcount      <<"\n";
+    m_fout.close();
+  }
+}
+
