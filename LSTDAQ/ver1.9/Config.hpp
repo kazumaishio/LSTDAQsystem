75a79e5820ab71d9f2b57bb9a4950f99c88cb729
Kazuma Ishio <kazumaishio@gmail.com>
2014-12-21 16:44:07 +0900

    LSTDAQ ver1.9
    2014/12/20 version
    Measure update before 2.0
    *** reads data and checks the format as below ***
    HEADER : FF    char
    IPADDR : 4byte
    EvntNo : 4byte unsigned int (produced by FakeFEB sequentially)
    TrigNo : 4byte unsigned int (produced by TrigGen sequentially)
    
    *** Additional funcs
    Stores last Ntrg,Nevt of each Conn
    Measurement start point is AFTER the 1st data is read.
    Error exit when Ntrg is much higher than Nread
    	(it also writes error message on measurement file)

---
+++ LSTDAQ/ver1.9/Config.hpp
@@ -0,0 +1,22 @@
+//
+//  Config.hpp
+//  
+//
+//  Created by 石尾 一馬 on 2014/12/19.
+//
+//
+
+#ifndef _Config_hpp
+#define _Config_hpp
+
+#define EVENTSIZE 976
+#define HEADERLEN 2
+#define IPADDRLEN 4
+#define EVTNOLEN  4
+#define TRGNOLEN  4
+
+#define MAX_CONNECTION 48
+#define MAX_RINGBUF 49
+
+#define ERR_NDROPPED 100000
+#endif
